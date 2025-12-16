af9a997a8595283f32122018726562c757fabae8
jinet-custodio <jeanettemaecustodio.basc@gmail.com>
2025-12-17 00:22:31 +0800

    database changed to postgre

--- app.py
+++ app.py
@@ -4,20 +4,22 @@ from dotenv import load_dotenv
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
-load_dotenv()  # Load .env file
-print(os.environ.get("SUPABASE_DB_URL_DEV"))
-print("FLASK_ENV:", os.getenv("FLASK_ENV"))
+load_dotenv()
 
 app = Flask(__name__)
 
-# Determine which DB to use
+# Environment
 FLASK_ENV = os.getenv("FLASK_ENV", "development")
-DB_URL = os.getenv("SUPABASE_DB_URL_DEV" if FLASK_ENV=="development" else "SUPABASE_DB_URL_PROD")
+
+DB_URL = os.getenv(
+    "DATABASE_URL_DEV" if FLASK_ENV == "development" else "DATABASE_URL_PROD"
+)
+
+if not DB_URL:
+    raise RuntimeError("Database URL not set. Check your .env file.")
 
 def get_db_connection():
-    conn = psycopg2.connect(DB_URL, cursor_factory=RealDictCursor)
-    return conn
-
+    return psycopg2.connect(DB_URL, cursor_factory=RealDictCursor)
 
 @app.route("/")
 def home():
@@ -29,14 +31,10 @@ def home():
     conn.close()
     return render_template("index.html", certificates=certificates)
 
-
 @app.route("/projects")
 def projects():
     return render_template("projects.html")
 
-
-# Runs the app only when executed directly
-# (not when imported by another file)
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
--- static/JS/certificates.js
+++ static/JS/certificates.js
@@ -7,3 +7,67 @@ imageModal.addEventListener("show.bs.modal", function (event) {
     const modalImage = document.getElementById("modalImage");
     modalImage.src = imgSrc;
 });
+
+//for sliding across certs
+function scrollSlider(direction) {
+    const slider = document.getElementById("certSlider");
+    const scrollAmount = 300; // pixels per click
+    slider.scrollLeft += direction * scrollAmount;
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    const allBtn = document.getElementById('allCerts');
+    const pyBtn = document.getElementById('pythonCerts');
+    const opSysBtn = document.getElementById('opSysCerts');
+    const cybersecBtn = document.getElementById('cybersecCerts');
+    const others = document.getElementById('other');
+    const allCerts = document.querySelectorAll('.cert');
+    const pyCerts = document.querySelectorAll('.pythonCert');
+    const opSysCerts = document.querySelectorAll('.OSCert');
+    const cybersecCerts = document.querySelectorAll('.cybersecCert');
+    const otherCerts = document.querySelectorAll('.others');
+
+    function hideall() {
+        allCerts.forEach((btn) => {
+            btn.style.display = 'none';
+        });
+    }
+
+    allBtn.addEventListener("click", (event) => {
+        hideall();
+        allCerts.forEach((btn) => {
+            btn.style.display = 'block';
+        });
+    });
+
+    pyBtn.addEventListener("click", (event) => {
+        hideall();
+        pyCerts.forEach((pyCert) => {
+            pyCert.style.display = 'block';
+        });
+    });
+
+    opSysBtn.addEventListener("click", (event) => {
+        hideall();
+        opSysCerts.forEach((OSCert) => {
+            OSCert.style.display = 'block';
+        });
+    });
+
+    cybersecBtn.addEventListener("click", (event) => {
+        hideall();
+        cybersecCerts.forEach((CSCert) => {
+            CSCert.style.display = 'block';
+        });
+    });
+
+    others.addEventListener("click", (event) => {
+        hideall();
+        otherCerts.forEach((OCert) => {
+            OCert.style.display = 'block';
+        });
+    });
+
+
+
+})
